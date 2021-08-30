b3e9b54fff13839215852edd8a7a2c0d951887e2
afpa <afpa@STA6302242.exchange.ad.afpanet>
2021-08-30 15:26:54 +0200

    correction donnée

--- donnee.sql
+++ donnee.sql
@@ -1,3 +1,3 @@
-INSERT INTO `ville` (`nom`, `numero_de_departement`,`code_postal`,`date_de_maj`) VALUES
+INSERT INTO `ville` (`nom`, `numerodedepartement`,`codepostal`,`datedemaj`) VALUES
 ('Tourcoing',59,59200,'2021-08-17'),
 ('Lille',59,59000,'2021-08-17');
