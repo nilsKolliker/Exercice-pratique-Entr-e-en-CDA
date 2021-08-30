f3bf654a4dd84129dc0cbf89e14671dfa7b588e8
afpa <afpa@STA6302242.exchange.ad.afpanet>
2021-08-30 15:41:50 +0200

    affichage des données

--- PHP/VIEW/accueil.php
+++ PHP/VIEW/accueil.php
@@ -6,7 +6,46 @@
             </div>
             <?php
                 $listeVille=VilleManager::getList();
-                var_dump($listeVille);
+                foreach ($listeVille as $ville) {
+                    echo"".
+                    "<div>".
+                        "<div>".
+                            "nom:".
+                        "</div>".
+                        "<div>".
+                            $ville->getNom().
+                        "</div>".
+                    "</div>".
+                    "<div>".
+                        "<div>".
+                            "numéro de département:".
+                        "</div>".
+                        "<div>".
+                            $ville->getNumeroDeDepartement().  
+                        "</div>".
+                    "</div>".
+                    "<div>".
+                        "<div>".
+                            "code postal:".
+                        "</div>".
+                        "<div>".
+                            $ville->getCodePostal().
+                        "</div>".
+                    "</div>".
+                    "<div>".
+                        "<div>".
+                            "dernière mise à jour:".
+                        "</div>".
+                        "<div>".
+                            $ville->getDateDeMaj().
+                        "</div>".
+                    "</div>".
+                    "<div class='spaceHorizon'></div>".
+                    
+                    "<div class='spaceHorizon'></div>".
+                    "<div class='spaceHorizon'></div>"
+                    ; 
+                }
             ?>
         
         </div>
